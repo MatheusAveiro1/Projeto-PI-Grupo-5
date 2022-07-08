77107fb51c0616832a698aa3e89e2757658f1ccd
Diya Shami <dyshami@gmail.com>
2022-07-08 00:01:25 -0300

    pagina produto transformada em ejs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var produtosRouter = require('./routes/produtos');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/produto', produtosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
