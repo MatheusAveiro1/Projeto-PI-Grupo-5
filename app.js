fb22d076eaa55b8a8dd414c423cc995909731b15
MatheusAveiro1 <matheusaveirodesouza1@gmail.com>
2022-07-15 21:55:01 -0300

    padronização mvc pagina login e cadastro ajuste de html e css

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var cadastroRouter = require('./routes/cadastro');
 
 var app = express();
 
@@ -21,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/login', loginRouter);
+app.use('/cadastro', cadastroRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ controllers/cadastroController.js
@@ -0,0 +1,7 @@
+const controlador = {
+    cadastro: (req, res)=> {
+      res.render ('cadastro')
+    }
+  }
+  
+  module.exports = controlador;
