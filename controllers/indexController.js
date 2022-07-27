d959fd383188fb22ceb5c89cb2272228da932c94
Alisson-Mendes <alissoncarlosmsj@gmail.com>
2022-07-27 00:15:46 -0300

    Mocados todos os dados do index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,18 @@
+const fs = require('fs');//metodo fs Manipulador de arquivos
+
 const controlador = {
   index: (req, res)=> {
-    res.render ('index')
+    let nomeDoArquivo = './database/produtos.json'
+    let produtos = JSON.parse(fs.readFileSync(nomeDoArquivo, 'utf-8'));// conversão do json para um objeto: fs read lê o arquivo, e json.parse converte o aruivo para objeto literal
+
+    
+    res.render ('index', {produtos: produtos})
+
+
   },
   produto: (req, res)=>{
     res.render('produto')
-  },
-
-  
-
+  }
 }
 
 module.exports = controlador;
