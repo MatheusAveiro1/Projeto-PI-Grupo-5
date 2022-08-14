ca78408ce2a234fe98f97237bfc0880106867ee2
Alisson-Mendes <alissoncarlosmsj@gmail.com>
2022-08-14 18:40:48 -0300

    foi criado a comunicação com o banco, atualizando estruturas de codigo

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -8,134 +8,119 @@ const {sequelize, Usuario} = require('../models')
 const bcrypt = require('bcrypt');
 const db = require('../models');
 
+//** Funções ***
+
+
+
+
+
 
 const controlador = {
   login: (req, res)=> {
-    res.render ('login')
+    res.render('login', {usuarioCadastrado:req.query.usuarioCadastrado});
   },
   cadastro: (req, res)=> {
     res.render ('cadastro')
   },
-  validaCadastro: (req, res)=> {
+  validaCadastro: async (req, res)=> {
 
-    //Recuperando possiveis erros do form
-    let errors = validationResult(req);
+    
 
-    //Verifica se houve erros no formulário, se sim, devolve os erros para que o usuário
-    if (!errors.isEmpty()) {
-      //Retornaremos para page de cadastro com os erros
-      return res.render('cadastro', { errorsFormCadastro: errors.mapped(), dadosPreenchido: req.body });
-    }
+      //Recuperando possiveis erros do form
+      let errors = validationResult(req);
 
-    async function verificaEmailExixtente(){
-      try {
-        //aguardando o sequilize criar o usuario no banco de dados
-        const usuarioVerificado = await Usuario.findAll({
-          where: {
-            email: req.body.email
-          }
-        })
-        
-
-        const resultadoUsuarioVerificado = usuarioVerificado.map(u => u.toJSON())
-
-        if (resultadoUsuarioVerificado[0].email) {
-         
-          //Excluir o avatar na pasta de upload            
-          fs.rm('./public/images/avatares/' + req.file.filename, { recursive:true }, (err) => {
-            if(err){
-                // Se a exclusão falhar
-                console.error(err.message);
-                return;
-            }                
-        });
-
-
-          return res.send('email existe')
-        }
-
-      }
-      catch (err) {
-        console.log(err)
+      //Verifica se houve erros no formulário, se sim, devolve os erros para que o usuário
+      if (!errors.isEmpty()) {
+        //Retornaremos para page de cadastro com os erros
+        return res.render('cadastro', { errorsFormCadastro: errors.mapped(), dadosPreenchido: req.body });
       }
 
-      
-   };
+      async function verificaEmailExixtente() {
+        try {
+          //Aguardando o sequilize buscar o usuario no banco de dados
+          const usuarioVerificado = await Usuario.findAll({
+            where: {
+              email: req.body.email
+            }
+          })
 
-   verificaEmailExixtente()
-    
-    function buscaInfoUsusarios () {
-      //return JSON.parse(fs.readFileSync(this.nomeDoArquivo, 'utf-8'));
-  }
+          //Organizando o array  do banco de dados
+          const resultadoUsuarioVerificado =  usuarioVerificado.map(u => u.toJSON())
+          //verifica email exitente dentro do banco
+          if (resultadoUsuarioVerificado[0].email) {
 
-    function buscaUsusarioPeloCampo (campo, value) {
-    
-      // let todosUsuarios = this.buscaInfoUsusarios();
-      // let ususrioProcurado = todosUsuarios.find(usuario => usuario[campo] === value);
-      // return ususrioProcurado;
-  }
-
-    //Verifica se o usuário existe
-    let usuarioExiste = buscaUsusarioPeloCampo('email', req.body.email);
-    if(usuarioExiste) {
-        //Excluir o avatar na pasta de upload            
-        fs.rm('./public/images/avatar/' + req.file.filename, { recursive:true }, (err) => {
-            if(err){
-                // Se a exclusão falhar
-                console.error(err.message);
-                return;
-            }                
-        });
-
-        //Retorna o erro para usuário
-        return res.render('cadastroUsuario', 
-        {
-            errors: {
-                email: { 
+            //se existir email igual, ele Verificando se o usuario fez algum upload
+            if (req.file) {
+              //Exclui o avatar na pasta de upload 
+              fs.rm('./public/images/avatares/' + req.file.filename, { recursive: true }, (err) => {
+                if (err) {
+                  // Se a exclusão falhar
+                  console.error(err.message);
+                  return;
+                }
+              });
+            }
+            //Retorna o erro para usuário
+            console.log("verificando usuario existente")
+            console.log("Usuario existente: " + resultadoUsuarioVerificado[0].email)
+            return res.render('cadastro',
+              {
+                errorsFormCadastro: {
+                  email: {
                     param: 'email',
-                    msg:'Este e-mail já existe na base de dados!'
-                },                   
-            },
-            dadosPreenchido: req.body
-        });
-    }
+                    msg: 'Este e-mail já existe na base de dados!'
+                  },
+                },
+                dadosPreenchido: req.body
+              });
 
-    let usuarioParaCriar = {}
-    //verificando se o usuario enviou foto
-    if (req.file === undefined) {
-      //Recupera os dados enviados via post pelo form, substitui a senha para senha criptografada e adiciona uma imagem default para o usuario
-      usuarioParaCriar = {
-        ...req.body,
-        senha: bcrypt.hashSync(req.body.senha, 10),
-        foto: "default.jpeg"
+          }
+        }
+        catch (err) {
+          console.log(err)
+        }
+      };
+      verificaEmailExixtente()
+
+      async function funcUsuarioParaCriar() {
+
+        let novoUsuarioParaCriar = {}
+        //verificando se o usuario enviou foto
+        if (req.file === undefined) {
+          //Recupera os dados enviados via post pelo form, substitui a senha para senha criptografada e adiciona uma imagem default para o usuario
+          novoUsuarioParaCriar = {
+            ...req.body,
+            senha: bcrypt.hashSync(req.body.senha, 10),
+            foto: "default.jpeg"
+          }
+        } else {
+          //Recupera os dados enviados via post pelo form, substitui a senha para senha criptografada e adiciona a imagem enviada pelo o usuario
+          novoUsuarioParaCriar = {
+            ...req.body,
+            senha: bcrypt.hashSync(req.body.senha, 10),
+            foto: req.file.filename
+          }
+        }
+        console.log("preparando usuario para criar no banco")
+        return novoUsuarioParaCriar
       }
-    } else {
-      //Recupera os dados enviados via post pelo form, substitui a senha para senha criptografada e adiciona a imagem enviada pelo o usuario
-      usuarioParaCriar = {
-        ...req.body,
-        senha: bcrypt.hashSync(req.body.senha, 10),
-        foto: req.file.filename
-      }
-    }
+      const usuarioParaCriar =  funcUsuarioParaCriar()
+
+      //Gravando dados na banco de dados - usuario
+      // async function criarUsuarios() {
+      //   try {
+      //     //aguardando o sequilize criar o usuario no banco de dados
+      //     await Usuario.create({ nome: usuarioParaCriar.nome, sobrenome: usuarioParaCriar.sobrenome, email: usuarioParaCriar.email, senha: usuarioParaCriar.senha, foto: usuarioParaCriar.foto })
+      //     return res.redirect('/usuarios/login?usuarioCadastrado=' + usuarioParaCriar.nome + ' ' + usuarioParaCriar.sobrenome);
+      //   }
+      //   catch (err) {
+      //     console.log(err)
+      //   }
+
+      // };
+      // criarUsuarios()
 
 
-    //gravando dados na banco de dados - usuario
-      async function criarUsuarios(){
-
-      try {
-        //aguardando o sequilize criar o usuario no banco de dados
-        await Usuario.create({ nome: usuarioParaCriar.nome, sobrenome: usuarioParaCriar.sobrenome, email: usuarioParaCriar.email, senha: usuarioParaCriar.senha, foto: usuarioParaCriar.foto })
-        
-        return res.send("Cadastro realizado com sucesso.");
-      }
-      catch (err) {
-        console.log(err)
-      }
-
-      
-   };
-    criarUsuarios()
-    
   },
   perfil: (req, res)=> {
     res.render ('perfil')
