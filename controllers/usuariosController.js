eec2508f80ebe433f91ea0e948f456a58e656fe8
Alisson-Mendes <alissoncarlosmsj@gmail.com>
2022-09-04 16:18:09 -0300

    Criação da registro de atualização de dados do usuario no banco de dados

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -8,6 +8,7 @@ const {sequelize, Usuario, Endereco} = require('../models')
 const bcrypt = require('bcrypt');
 const db = require('../models');
 const { reject, promise } = require('bcrypt/promises');
+const { resolve } = require('path');
 
 
 //** Funções ***//
@@ -167,10 +168,13 @@ const funcoesUsuarios = {
         if (req.file === undefined) {
           //Recupera os dados enviados via post pelo form, substitui a senha para senha criptografada e adiciona uma imagem default para o usuario
           resolve(novoUsuario = {
+            
             ...req.body,
             senha: bcrypt.hashSync(req.body.senha, 10),
             foto: "default.jpeg"
-          })
+          }
+          
+          )
         } else {
           //Recupera os dados enviados via post pelo form, substitui a senha para senha criptografada e adiciona a imagem enviada pelo o usuario
           resolve (novoUsuario = {
@@ -248,9 +252,13 @@ const controlador = {
       //Valida se o CPF e o EMAIL já existe no BD
       await funcoesUsuarios.validaCpfEmailExistente(req,res,cpfParaVerificar,emailParaVerificar);
       
+
+
       //Cria um objeto com as informações do novo usuário
       const usuarioParaCriarNoBd = await funcoesUsuarios.criarUsuarioParaCadastrarNoBd(req,res);      
       
+
+
       //Gravando os dados do novo usuário no BD           
       const usuarioCriadoNoBd = await Usuario.create({nome: usuarioParaCriarNoBd.nome,sobrenome: usuarioParaCriarNoBd.sobrenome,email: usuarioParaCriarNoBd.email,senha: usuarioParaCriarNoBd.senha,foto: usuarioParaCriarNoBd.foto,cpf: usuarioParaCriarNoBd.cpf});
       
@@ -269,9 +277,49 @@ const controlador = {
   },
   meusDados: (req, res)=> {
     const meusDados = req.session.usuarioLogado
-
+    console.log('>>>>>>>teste meus dados<<<<<<<<')
+    console.log(meusDados)
    return res.render ('meus-dados', {meusDados: meusDados, paginaAtual: 'meusDados'})
   },
+
+  atualizarMeusDados: async (req, res) =>{
+
+      try{
+
+  
+
+
+        const resultadoDoEnvioDeDadosDoBanco =  await Usuario.update({
+          nome: req.body.nome,
+          sobrenome: req.body.sobrenome,
+          email: req.session.usuarioLogado.email,
+          cpf: req.body.cpf,
+          rg: req.body.rg,
+          telefone: req.body.tel,
+          data_nascimento: req.body.data_nasc
+
+
+        },
+        { where: { id: req.session.usuarioLogado.id} }
+        )
+
+        
+
+
+        return res.redirect('/usuario/meus-dados'); 
+        
+      }   
+      catch (err) {
+          console.log(err)
+      }
+
+    },
+
+
+
+   
+  
+
   mostraEnderecos: async (req, res) =>{
 
     try{
