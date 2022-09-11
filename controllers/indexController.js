1049f4c9d31b301aa7328bd0af03d76f09367713
Diya Shami <dyshami@gmail.com>
2022-09-11 20:52:22 -0300

    pagina-produto-atualizada-para-banco-de-dados

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,5 @@
 const fs = require('fs');//metodo fs Manipulador de arquivos
-const {sequelize, Produto} = require('../models');
+const {sequelize, Produto, Categoria} = require('../models');
 
 const controlador = {
   index: async (req, res)=> {
@@ -24,25 +24,46 @@ const controlador = {
   produto: async (req, res)=>{
 
     try {
-      //let nomeDoArquivo = './database/produtos.json'
+      
       //pegando id que esta vendo da pagina index
       let id = req.params.id;
       // buscando informacoes de produto banco de dados
       let infoProduto = await Produto.findByPk (id, {
+      
         include: ['produto_categoria', 'produto_marca']
       })
       infoProduto = infoProduto.dataValues
-        //console.log (infoProduto)
+        
 
-    res.render('produto',{produto: infoProduto , idProduto: id, carrinho: req.session.carrinho})
+        //pegando o id da categoria do produto
+        let categoriaProduto = infoProduto.produto_categoria.id
+        //procurando produtos da mesma cartegoria
+        let maisProduto = await Produto.findAll ( 
+          
+          {
+          limit: 4,
+          where: {id_categoria: categoriaProduto}  
+          
+        }
+
+        )
+        
+
+
+
+    res.render('produto',{produto: infoProduto , idProduto: id, produtosRelacionados: maisProduto, carrinho: req.session.carrinho})
 
 
     }
     catch (err){
       console.log(err)
-    //let produtos = JSON.parse(fs.readFileSync(nomeDoArquivo, 'utf-8')); // conversão do json para um objeto: fs read lê o arquivo, e json.parse converte o aruivo para objeto literal
-    }
+      }
   }
+    
+
+  
+
+  
 }
 
 module.exports = controlador;
